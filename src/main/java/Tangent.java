58c0f4de159caa265e1005577f512e04238367e6
arcs1ne <60043450+arcs1ne@users.noreply.github.com>
2026-08-19 11:17:53 +0800

    implemented list of tasks

--- src/main/java/Tangent.java
+++ src/main/java/Tangent.java
@@ -13,17 +13,33 @@ public class Tangent {
         System.out.println("good morning/afternoon/evening ^-^ I'm TANGENT.\nwhat do you want me to do?");
         System.out.println(DIVIDER);
 
+        String[] tasks = new String[100];
+        int numItems = 0;
         try (Scanner scanner = new Scanner(System.in)) {
             while (scanner.hasNextLine()) {
                 String command = scanner.nextLine();
-                System.out.println(command);
-                System.out.println(DIVIDER);
+                if (command.equals("list")) {
+                    for (int i = 0; i < numItems; i++) {
+                        System.out.println((i + 1) + ". " + tasks[i]);
+                    }
+                    System.out.println(DIVIDER);
+                    continue;
+                }
 
                 if (command.equals("bye")) {
                     System.out.println("bye o/ hope to see you again soon");
                     System.out.println(DIVIDER);
                     return;
                 }
+                if (numItems >= tasks.length) {
+                    System.out.println("Sorry, the task list is full.");
+                    System.out.println(DIVIDER);
+                    continue;
+                }
+                System.out.println("i've added: " + command);
+                tasks[numItems] = command;
+                numItems++;
+                System.out.println(DIVIDER);
             }
         }
     }
